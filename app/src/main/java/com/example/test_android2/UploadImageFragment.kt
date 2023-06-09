f2a1296bf7ddf02966ff6e1307c722acc5d3dc0a
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-06-09 17:13:17 +0900

    [feat] 문장 분석 기능 구현

--- app/src/main/java/com/example/test_android2/UploadImageFragment.kt
+++ app/src/main/java/com/example/test_android2/UploadImageFragment.kt
@@ -1,11 +1,19 @@
 package com.example.test_android2
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.test_android2.data.ChatData
+import com.example.test_android2.data.ResponseChat
+import com.example.test_android2.data.ServiceCreator
 import com.example.test_android2.databinding.FragmentUploadImageBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class UploadImageFragment : Fragment() {
 
@@ -20,4 +28,39 @@ class UploadImageFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initButtonClickEvent()
+
+    }
+
+    private fun initButtonClickEvent() = binding.btnAnalysis.setOnClickListener{
+        val chatWords = binding.etChatWords.text.toString()
+        val chatData = ChatData(chatWords)
+        chatNetwork(chatData)
+    }
+
+    private fun chatNetwork(chatInfo: ChatData) {
+        val call: Call<ResponseChat> = ServiceCreator.chatService.uploadChat(chatInfo)
+
+        call.enqueue(object : Callback<ResponseChat> {
+            override fun onResponse(
+                call: Call<ResponseChat>, response: Response<ResponseChat>
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        val resultNum = it.resultNum
+                        val intent = Intent(context, ResultAnalysisActivity::class.java)
+                        intent.putExtra("resultNum", resultNum.toString())
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseChat>, t: Throwable) {
+                Log.d("문장 분석 실패", t.message.toString())
+            }
+        })
+    }
 }
---
+++ app/src/main/java/com/example/test_android2/data/ResponseChat.kt
@@ -0,0 +1,9 @@
+package com.example.test_android2.data
+
+data class ResponseChat(
+    val error: String?,
+    val userId: String?,
+    val chatId: String?,
+    val resultNum: Int?
+)
+
