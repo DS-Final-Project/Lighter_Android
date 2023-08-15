43a1695353bdb7da7dc4dc300b55461fa2c2c3e0
kang <guswjd9994@duksung.ac.kr>
2023-08-15 21:34:41 +0900

    [feat] 정보페이지 UI

--- app/src/main/java/com/example/test_android2/InfoFragment.kt
+++ app/src/main/java/com/example/test_android2/InfoFragment.kt
@@ -1,27 +1,29 @@
-package com.example.test_android2
-
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
-import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
-
+import com.example.test_android2.databinding.FragmentInfoBinding
+import com.example.test_android2.cardviewAdapter
 
 class InfoFragment : Fragment() {
-    lateinit var textView: TextView
+    private var _binding: FragmentInfoBinding? = null // 뷰 바인딩 변수 선언
+    private val binding get() = _binding!! // 뷰 바인딩 가져오기
     private lateinit var viewPager: ViewPager2
     private lateinit var adapter: cardviewAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_info, container, false)
-        viewPager = view.findViewById(R.id.view_pager2)
+        _binding = FragmentInfoBinding.inflate(inflater, container, false) // 뷰 바인딩 인플레이트
+        val view = binding.root // 뷰 바인딩으로 뷰 참조
+
+        viewPager = binding.viewPager2 // 뷰 바인딩으로 뷰 참조
 
         val models: MutableList<String> = mutableListOf()
         models.add("친구 간 대화방식이\n 고민인가요? ")
@@ -29,31 +31,52 @@ class InfoFragment : Fragment() {
         models.add("나는 왜 관계가\n 어려울까?")
         models.add("불안정 애착\n 극복하기")
 
-        textView = view.findViewById(R.id.tv_1)
-        val textData: String = textView.text.toString()
-        val builder = SpannableStringBuilder(textData)
-        val colorBlueSpan = ForegroundColorSpan(Color.rgb(244,172,63))
-        builder.setSpan(colorBlueSpan, 0, 3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        textView.text = builder
+        val dpValue = 54
+        val d = resources.displayMetrics.density
+        val margin = (dpValue * d).toInt()
 
         adapter = cardviewAdapter(models, requireContext())
         viewPager.adapter = adapter
-        viewPager.setPadding(30, 0, 30, 0)
+        viewPager.setClipToPadding(false)
+        viewPager.setPadding(margin, 0, margin, 0)
+        //viewPager.pageMargin = margin/2;
 
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-            }
+        val textView = binding.tv1
+        val textData: String = textView.text.toString()
+        val builder = SpannableStringBuilder(textData)
+        val colorBlueSpan = ForegroundColorSpan(Color.rgb(244,172,63))
 
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-            }
+        builder.setSpan(colorBlueSpan, 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = builder
 
-            override fun onPageScrollStateChanged(state: Int) {
-                super.onPageScrollStateChanged(state)
-            }
-        })
+        setupButtonClickListeners() // 버튼 클릭 리스너 설정
 
         return view
+    }
+
+    private fun setupButtonClickListeners() {
+        val button1 = binding.call1
+        val button2 = binding.call2
+        val button3 = binding.call3
+
+        button1.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:1388"))
+            startActivity(myIntent)
+        }
+
+        button2.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:02-2285-1318"))
+            startActivity(myIntent)
+        }
+
+        button3.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:02-3156-5400"))
+            startActivity(myIntent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // 뷰 바인딩 해제
     }
 }
