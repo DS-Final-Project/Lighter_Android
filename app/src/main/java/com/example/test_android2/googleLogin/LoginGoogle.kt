28082d3cef4fb24e1fde798d3377d1b76c3945bd
kang <guswjd9994@duksung.ac.kr>
2024-03-08 16:45:37 +0900

    [fix] 불필요한 파일, 코드 삭제

--- app/src/main/java/com/example/test_android2/googleLogin/LoginGoogle.kt
+++ app/src/main/java/com/example/test_android2/googleLogin/LoginGoogle.kt
@@ -2,39 +2,24 @@ package com.example.test_android2.googleLogin
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
-import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 
 class LoginGoogle(context: Context) {
+    val RC_SIGN_IN = 1001
 
     private val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken("990677384481-pr02jubof69av8ponrt46eok0c7grmrg.apps.googleusercontent.com")
-        .requestServerAuthCode("990677384481-pr02jubof69av8ponrt46eok0c7grmrg.apps.googleusercontent.com")
+        .requestIdToken("945444302382-7b29vcb2rdqga8cmrnat5r48i4ri7ndu.apps.googleusercontent.com")
+        .requestServerAuthCode("945444302382-7b29vcb2rdqga8cmrnat5r48i4ri7ndu.apps.googleusercontent.com")
         .requestEmail()
         .build()
 
     private val googleSignInClient = GoogleSignIn.getClient(context, gso)
 
-    fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val authCode: String? =
-                completedTask.getResult(ApiException::class.java)?.serverAuthCode
-            LoginRepository().getAccessToken(authCode!!)
-        } catch (e: ApiException) {
-            Log.w(TAG, "handleSignInResult: error" + e.statusCode)
-        }
-    }
-
     fun signIn(activity: Activity) {
-        val signInIntent: Intent = googleSignInClient.signInIntent
-        activity.startActivityForResult(signInIntent, 1000)
-
+        val signInIntent = googleSignInClient.signInIntent
+        activity.startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     fun signOut(context: Context) {
---
+++ app/src/main/java/com/example/test_android2/info/ui/CardviewAdapter.kt
@@ -0,0 +1,175 @@
+package com.example.test_android2.info.ui
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.test_android2.R.id
+import com.example.test_android2.R.layout
+import com.example.test_android2.info.data.Solution
+
+class cardviewAdapter(
+    private var solutions: MutableList<Solution?>,
+    var context: Context
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        private const val VIEW_TYPE_FRIEND = 0
+        private const val VIEW_TYPE_LOVER = 1
+        private const val VIEW_TYPE_FAMILY = 2
+        private const val VIEW_TYPE_COLLEAGUE = 3
+        private const val VIEW_TYPE_DEFAULT = 4
+    }
+
+    override fun getItemCount(): Int {
+        return solutions.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val relation = solutions[position]?.relation
+        return when (relation) {
+            1 -> VIEW_TYPE_LOVER
+            2 -> VIEW_TYPE_FRIEND
+            3 -> VIEW_TYPE_FAMILY
+            4 -> VIEW_TYPE_COLLEAGUE
+            else -> VIEW_TYPE_DEFAULT
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            VIEW_TYPE_FRIEND -> {
+                val friendView = inflater.inflate(layout.cardview_friend, parent, false)
+                FriendViewHolder(friendView)
+            }
+            VIEW_TYPE_LOVER -> {
+                val loverView = inflater.inflate(layout.cardview_lover, parent, false)
+                LoverViewHolder(loverView)
+            }
+            VIEW_TYPE_FAMILY -> {
+                val familyView = inflater.inflate(layout.cardview_family, parent, false)
+                FamilyViewHolder(familyView)
+            }
+            VIEW_TYPE_COLLEAGUE -> {
+                val colleagueView = inflater.inflate(layout.cardview_colleague, parent, false)
+                ColleagueViewHolder(colleagueView)
+            }
+            else -> {
+                val defaultView = inflater.inflate(layout.cardview_none, parent, false)
+                DefaultViewHolder(defaultView)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val solution = solutions[position]
+
+        when (holder) {
+            is FriendViewHolder -> {
+                if (solution != null) {
+                    holder.title.text = solution.solutionTitle
+                    holder.relation.text = "친구"
+                    holder.keyword.text = solution.keyword
+
+                    holder.itemView.setOnClickListener {
+                        // 클릭한 카드뷰의 내용을 보여주는 작업을 여기에 추가
+                        // 예를 들어, 다음과 같이 Intent를 사용하여 다른 화면으로 이동할 수 있습니다.
+                        val intent = Intent(context, SolutionActivity::class.java)
+                        intent.putExtra("clickSolution", solution) // 선택한 솔루션 데이터 전달
+                        context.startActivity(intent)
+                    }
+                }
+            }
+            is LoverViewHolder -> {
+                if (solution != null) {
+                    holder.title.text = solution.solutionTitle
+                    holder.relation.text = "연인"
+                    holder.keyword.text = solution.keyword
+
+                    holder.itemView.setOnClickListener {
+                        // 클릭한 카드뷰의 내용을 보여주는 작업을 여기에 추가
+                        val intent = Intent(context, SolutionActivity::class.java)
+                        intent.putExtra("clickSolution", solution) // 선택한 솔루션 데이터 전달
+                        Log.d("연인 솔루션 클릭시 솔루션:", solution.solutionId.toString())
+                        context.startActivity(intent)
+                    }
+                }
+            }
+            is FamilyViewHolder -> {
+                if (solution != null) {
+                    holder.title.text = solution.solutionTitle
+                    holder.relation.text = "가족"
+                    holder.keyword.text = solution.keyword
+
+                    holder.itemView.setOnClickListener {
+                        // 클릭한 카드뷰의 내용을 보여주는 작업을 여기에 추가
+                        val intent = Intent(context, SolutionActivity::class.java)
+                        intent.putExtra("clickSolution", solution) // 선택한 솔루션 데이터 전달
+                        context.startActivity(intent)
+                    }
+                }
+            }
+            is ColleagueViewHolder -> {
+                if (solution != null) {
+                    holder.title.text = solution.solutionTitle
+                    holder.relation.text = "동료"
+                    holder.keyword.text = solution.keyword
+
+                    holder.itemView.setOnClickListener {
+                        // 클릭한 카드뷰의 내용을 보여주는 작업을 여기에 추가
+                        val intent = Intent(context, SolutionActivity::class.java)
+                        intent.putExtra("clickSolution", solution) // 선택한 솔루션 데이터 전달
+                        context.startActivity(intent)
+                    }
+                }
+            }
+            is DefaultViewHolder -> {
+                // DefaultViewHolder에 대한 처리 추가
+                Toast.makeText(context, "먼저 채팅을 등록해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    fun addCardView(solution: Solution?) {
+        if (solution != null) {
+            solutions.add(solution)
+            notifyItemInserted(solutions.size - 1)
+        }
+    }
+
+    inner class FriendViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(id.title)
+        val relation: TextView = view.findViewById(id.tag1)
+        val keyword: TextView = view.findViewById(id.tag2)
+    }
+
+    inner class LoverViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(id.title)
+        val relation: TextView = view.findViewById(id.tag1)
+        val keyword: TextView = view.findViewById(id.tag2)
+    }
+
+    inner class FamilyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(id.title)
+        val relation: TextView = view.findViewById(id.tag1)
+        val keyword: TextView = view.findViewById(id.tag2)
+    }
+
+    inner class ColleagueViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(id.title)
+        val relation: TextView = view.findViewById(id.tag1)
+        val keyword: TextView = view.findViewById(id.tag2)
+    }
+
+    inner class DefaultViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
